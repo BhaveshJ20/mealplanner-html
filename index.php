bfe331a41fa5c987b4c55971df8bd4119f47f0dd
bhavesh <bjariwala@plusonex.com>
2023-04-26 17:27:36 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,353 @@
+<?php 
+    include("header_common.php");
+?>
+<!-- Header Start -->
+<header class="bg-white sticky-top">
+    <?php include("header-normal.php"); ?>
+</header>
+<!-- Header End -->
+
+<!-- Pages -->
+<section class="py-5">
+    <div class="container">
+        <div class="title text-center mb-4">
+            <h1 class="title-md text-primary fw-bold">Meal Planner Pages</h1>
+        </div>
+        <div class="table-responsive mb-5">
+            <table class="table bg-white table-bordered table-striped">
+                <thead class="bg-primary">
+                    <tr>
+                        <th scope="col lead">Screen Name</th>
+                        <th scope="col lead">Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Weekly Planner Styleguide</h5>
+                        </td>
+                        <td><a href="./styleguide.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="title text-center mb-4">
+            <h2 class="title-sm fw-bold">Approval of Workflow 2</h2>
+        </div>
+        <div class="table-responsive mb-5">
+            <table class="table bg-white table-bordered table-striped">
+                <thead class="bg-primary">
+                    <tr>
+                        <th scope="col lead">Screen Name</th>
+                        <th scope="col lead">Status</th>
+                        <th scope="col lead">Sign</th>
+                        <th scope="col lead">Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Weekly Planner</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./weekly-planner.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Weekly Planner Day</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./weekly-planner-day.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Weekly Planner Day 1</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./weekly-planner-day1.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Weekly Planner Add Recipe</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./weekly-planner-add-recipe.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Weekly Planner Recipe List</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./weekly-planner-recipe-list.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Weekly Planner Recipe List Change</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./weekly-planner-recipe-list-change.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Weekly Planner Recipe Details Add</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./weekly-planner-recipe-details.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>No Shopping List</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./noshopping-list.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Prev Shopping List_Key Ingredients</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./prev-shopping-list.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Create/Edit New Shopping List</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./create-new-shopping-list.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Edit Shopping List Create a day</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium"></td>
+                        <td><a href="./edit-shopping-list.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="title text-center mb-4">
+            <h2 class="title-sm fw-bold">Approval of Workflow 1</h2>
+        </div>
+        <div class="table-responsive">
+            <table class="table bg-white table-bordered table-striped">
+                <thead class="bg-primary">
+                    <tr>
+                        <th scope="col lead">Screen Name</th>
+                        <th scope="col lead">Status</th>
+                        <th scope="col lead">Sign</th>
+                        <th scope="col lead">Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire Welcome</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire-welcome.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 1</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire1.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 2</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire2.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 3</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire3.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 4-5</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire4-5.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 6</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire6.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 7</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire7.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 8</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire8.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 8-1-2</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire8-1-2.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 9</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire9.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 10-11</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire10-11.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Questionnaire 12</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./questionnaire12.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Login</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./login.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Login demo align</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./login-demo-align.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Login Error</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./login-error.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Forgot Password</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./forgot-password.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Reset Password</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./reset-password.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Registration</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./registration.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Verify E-mail address</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./email-verification.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Privacy Policy</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./privacy-policy.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>Terms and Condition</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./terms-condition.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>My Profile Payment For trial user</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./myprofile-trial-user.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>My Profile</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./myprofile.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                    <tr>
+                        <td class="text-dark">
+                            <h5>My Profile Checkout</h5>
+                        </td>
+                        <td class="text-success lead fw-medium">Done</td>
+                        <td class="text-gray lead fw-medium">Signed-off</td>
+                        <td><a href="./myprofile-checkout.php" target=_blank class="link-danger lead fw-medium">Link</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
+
+<?php
+    include("footer_common.php");
+?>
